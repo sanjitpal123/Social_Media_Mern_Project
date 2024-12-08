0a80c5a89edeae987f52b7416c4ae7aa10abf0aa
sanjitssp <manjitpal895@gmail.com>
2024-12-08 14:02:17 +0530

    Added front-end file

---
+++ front-end/socialmedia/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='w-[200px] h-[300px] bg-red-300'>
+      <p>flkdf</p>
+      
+     
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/socialmedia/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This ensures Tailwind CSS can find your classes in React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
